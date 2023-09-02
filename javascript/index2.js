7ba6240ef746e2788350cc1d583d341e77dfa4af
Mony <139051781+Mony-JR@users.noreply.github.com>
2023-09-02 20:50:05 +0700

    Add files via upload

---
+++ javascript/index2.js
@@ -0,0 +1,82 @@
+export const profile=[
+    {
+        name:"Mony JR"
+        ,sex:"Male"
+        ,phone:"086936457"
+        ,add:"Phnom Phenh"
+        ,img:"/img/pic/ny_me.jpg"
+        ,money:5000
+    }
+]
+
+
+export const pro = [
+    
+    {
+        id:'1',
+        img: "img/pic/coffee.png",
+        name: "coffee",
+        title: "",
+        price: 2.6,
+    },
+    
+    {
+        id:'9',   
+        img: "img/pic/coffee.png",
+        name: "coffee",
+        title: "",
+        price: 2.2,
+    },
+    {
+        id:'2',
+        img: "img/pic/green .webp",
+        name: "Green tea",
+        title: "This green tea",
+        price: 2.0,
+    },
+    {
+        id:'8',
+        img: "img/pic/OREO.webp",
+        name: "Cake",
+        title: "This green tea",
+        price: 0.5,
+    },
+    {
+        id:3,
+        img: "img/pic/Lyly.jpeg",
+        name: "Cake",
+        title: "This green tea",
+        price: 1.2,
+    },
+    {
+        id:4,
+        img: "img/pic/swinh.jpeg",
+        name: "Cake",
+        title: "This green tea",
+        price: 0.5,
+    },
+    {
+        id:5,
+        img: "img/pic/Coca.avif",
+        name: "Coca",
+        title: "This green tea",
+        price: 1.5,
+    },
+    {
+        id:6,
+        img: "img/pic/cake1.webp",
+        name: "Cake",
+        title: "This green tea",
+        price: 0.5,
+    },
+    {
+        id:7,
+        img: "img/pic/cake2.jpeg",
+        name: "Cake",
+        title: "This green tea",
+        price: 1.0,
+    },
+];
+
+
+
